6efc9682c95f6db7daec286175caf6147ba4b6d9
Rajdeep Barman <deepBn9@gmail.com>
2018-07-06 13:22:48 +0530

    added cancel and confirm button to order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,14 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  };
+
+  purchaseContinueHandler = () => {
+    alert('You continue');
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -77,7 +85,10 @@ class BurgerBuilder extends Component {
     }
     return <Aux>
       <Modal show={this.state.purchasing}>
-        <OrderSummary ingredients={this.state.ingredients}/>
+        <OrderSummary
+          ingredients={this.state.ingredients}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchaseContinueHandler}/>
       </Modal>
       <Burger ingredients={this.state.ingredients}/>
       <BuildControls
