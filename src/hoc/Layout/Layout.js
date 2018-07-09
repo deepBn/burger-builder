d0f2c8af15d986d6d9a90366fee4ef33f5b32e91
Rajdeep Barman <deepBn9@gmail.com>
2018-07-09 19:18:38 +0530

    refactor folders

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/Sidedrawer';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/Sidedrawer';
 import classes from './Layout.css';
 
 class Layout extends Component {
