926efa973dbc5df22d1b8ce5df76fcfcae8d3f31
Rajdeep Barman <deepBn9@gmail.com>
2018-07-16 19:26:56 +0530

    working on contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 
@@ -10,7 +11,34 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
-    }
+    },
+    loading: false
+  };
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({loading: true});
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Deep Bn',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: 465645,
+          country: 'India'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order)
+      .then(response => {
+        this.setState({loading: false, purchasing: false});
+      })
+      .catch(error => {
+        this.setState({loading: false, purchasing: false});
+      });
   };
 
   render() {
@@ -22,7 +50,7 @@ class ContactData extends Component {
           <input type="email" name="email" placeholder="Your Email"/>
           <input type="text" name="street" placeholder="Street"/>
           <input type="text" name="postal" placeholder="Postal Code"/>
-          <Button btnType="Success">ORDER</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
       </div>
     );
