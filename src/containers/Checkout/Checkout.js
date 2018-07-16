dd2582fb8f98ee4b1ca3713edf4e1f97c6cd57c4
Rajdeep Barman <deepBn9@gmail.com>
2018-07-16 17:01:41 +0530

    added contact data form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+import {Route} from 'react-router-dom';
 
 class Checkout extends Component {
   state = {
@@ -31,13 +33,13 @@ class Checkout extends Component {
   };
 
   render() {
-    console.log(this.state.ingredients);
     return (
       <div>
         <CheckoutSummary
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
           ingredients={this.state.ingredients}/>
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
       </div>
     );
   }
