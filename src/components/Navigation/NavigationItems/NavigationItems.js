42aa93d9ac57a1af04dd50e3397daa66cad3a90b
Rajdeep Barman <deepBn9@gmail.com>
2018-07-30 17:48:03 +0530

    auth form validation working

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,10 +3,11 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.NavigationItems}>
-    <NavigationItem exact link="/">Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
-  </ul>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem exact link="/">Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
+    </ul>
 );
 
 export default navigationItems;
